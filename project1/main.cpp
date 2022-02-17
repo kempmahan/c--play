0a07f2cceb79bda0f044774cd27f9018ce834da7
Kemper <kemper.mahanes@amprobotics.com>
2022-02-16 17:59:50 -0700

    add ignore

---
+++ project1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+   int favorite_number;
+   std::cout << "Enter favorite number between 1 and 100. :" ;
+   std::cin >> favorite_number;
+   std::cout << "Cool number bro";
+   return 0;
+}
